00367e96c894a96c094e53ec6e6da0f3ff16592e
Sameer Naik <sameer@damagehead.com>
2015-08-10 10:42:22 +0530

    create functions for better maintenence and readability

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,38 +2,55 @@
 set -e
 
 ROOT_PASSWORD=${ROOT_PASSWORD:-password}
+WEBMIN_ENABLED=${WEBMIN_ENABLED:-true}
 
-# script variables
 BIND_DATA_DIR=${DATA_DIR}/bind
 WEBMIN_DATA_DIR=${DATA_DIR}/webmin
 
-# configuration variables
-WEBMIN_ENABLED=${WEBMIN_ENABLED:-true}
+create_bind_data_dir() {
+  mkdir -p ${BIND_DATA_DIR}
+  chmod -R 0755 ${BIND_DATA_DIR}
+  chown -R root:${BIND_USER} ${BIND_DATA_DIR}
 
-## ...and here we go
-chmod 755 ${DATA_DIR}
+  # populate default bind configuration if it does not exist
+  if [ ! -d ${BIND_DATA_DIR}/etc ]; then
+    mv /etc/bind ${BIND_DATA_DIR}/etc
+  fi
+  rm -rf /etc/bind
+  ln -sf ${BIND_DATA_DIR}/etc /etc/bind
 
-# create directory for bind config
-mkdir -p ${BIND_DATA_DIR}
-chown -R root:${BIND_USER} ${BIND_DATA_DIR}
+  if [ ! -d ${BIND_DATA_DIR}/lib ]; then
+    mkdir -p ${BIND_DATA_DIR}/lib
+    chown root:${BIND_USER} ${BIND_DATA_DIR}/lib
+  fi
+  rm -rf /var/lib/bind
+  ln -sf ${BIND_DATA_DIR}/lib /var/lib/bind
+}
 
-# populate default bind configuration if it does not exist
-if [ ! -d ${BIND_DATA_DIR}/etc ]; then
-  mv /etc/bind ${BIND_DATA_DIR}/etc
-fi
-rm -rf /etc/bind
-ln -sf ${BIND_DATA_DIR}/etc /etc/bind
+create_webmin_data_dir() {
+  mkdir -p ${WEBMIN_DATA_DIR}
+  chmod -R 0755 ${WEBMIN_DATA_DIR}
+  chown -R root:root ${WEBMIN_DATA_DIR}
 
-if [ ! -d ${BIND_DATA_DIR}/lib ]; then
-  mkdir -p ${BIND_DATA_DIR}/lib
-  chown root:${BIND_USER} ${BIND_DATA_DIR}/lib
-fi
-rm -rf /var/lib/bind
-ln -sf ${BIND_DATA_DIR}/lib /var/lib/bind
+  # populate the default webmin configuration if it does not exist
+  if [ ! -d ${WEBMIN_DATA_DIR}/etc ]; then
+    mv /etc/webmin ${WEBMIN_DATA_DIR}/etc
+  fi
+  rm -rf /etc/webmin
+  ln -sf ${WEBMIN_DATA_DIR}/etc /etc/webmin
+}
 
-# create /var/run/named
-mkdir -m 0775 -p /var/run/named
-chown root:${BIND_USER} /var/run/named
+set_root_passwd() {
+  echo "root:$ROOT_PASSWORD" | chpasswd
+}
+
+create_pid_dir() {
+  mkdir -m 0775 -p /var/run/named
+  chown root:${BIND_USER} /var/run/named
+}
+
+create_pid_dir
+create_bind_data_dir
 
 # allow arguments to be passed to named
 if [[ ${1:0:1} = '-' ]]; then
@@ -47,19 +64,8 @@ fi
 # default behaviour is to launch named
 if [ -z "$@" ]; then
   if [ "${WEBMIN_ENABLED}" == "true" ]; then
-    # create directory for webmin
-    mkdir -p ${WEBMIN_DATA_DIR}
-
-    # populate the default webmin configuration if it does not exist
-    if [ ! -d ${WEBMIN_DATA_DIR}/etc ]; then
-      mv /etc/webmin ${WEBMIN_DATA_DIR}/etc
-    fi
-    rm -rf /etc/webmin
-    ln -sf ${WEBMIN_DATA_DIR}/etc /etc/webmin
-
-    # set root user password
-    echo "root:$ROOT_PASSWORD" | chpasswd
-
+    create_webmin_data_dir
+    set_root_passwd
     echo "Starting webmin..."
     /etc/init.d/webmin start
   fi
