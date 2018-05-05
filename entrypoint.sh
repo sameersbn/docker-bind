fe1003fb563998230df4c2df3ecfcb7fb49e829b
Carston Schilds <carstonschilds@gmail.com>
2018-05-05 15:32:09 -0700

    added support for docker secrets to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 set -e
 
-ROOT_PASSWORD=${ROOT_PASSWORD:-password}
-WEBMIN_ENABLED=${WEBMIN_ENABLED:-true}
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+  local var="$1"
+  local fileVar="${var}_FILE"
+  local def="${2:-}"
+  if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+    echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+    exit 1
+  fi
+  local val="$def"
+  if [ "${!var:-}" ]; then
+    val="${!var}"
+  elif [ "${!fileVar:-}" ]; then
+    val="$(< "${!fileVar}")"
+  fi
+  export "$var"="$val"
+  unset "$fileVar"
+}
+
+file_env 'ROOT_PASSWORD' 'password'
+file_env 'WEBMIN_ENABLED' 'true'
 
 BIND_DATA_DIR=${DATA_DIR}/bind
 WEBMIN_DATA_DIR=${DATA_DIR}/webmin
